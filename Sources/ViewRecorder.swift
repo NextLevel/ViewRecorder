9cf0bcfa28bf5311d3cdc17b5add84d2344a5ec0
patrick piemonte <patrick.piemonte@gmail.com>
2020-12-12 01:58:51 -0800

    add VideoWriter

--- Sources/ViewRecorder.swift
+++ Sources/ViewRecorder.swift
@@ -26,6 +26,7 @@
 
 import UIKit
 import Foundation
+import AVFoundation
 
 public enum ViewRecorderError: Error, CustomStringConvertible {
     case cancelled
@@ -43,6 +44,7 @@ public enum ViewRecorderError: Error, CustomStringConvertible {
     }
 }
 
+/// https://github.com/NextLevel/ViewRecorder
 public final class ViewRecorder {
     
     // MARK: - types
@@ -157,3 +159,163 @@ extension ViewRecorder {
     
 }
 
+public final class VideoWriter {
+ 
+    // MARK: - config
+    
+    public var outputSize: CGSize = .zero
+    public var outputFileUrl: URL?
+
+    public var framesPerSecond: Int32 = 120
+    public var startDate: Date?
+    public var endDate: Date?
+    
+    // MARK: - ivars
+    
+    fileprivate var _firstFrame: CFAbsoluteTime?
+    fileprivate var _timestamp: TimeInterval?
+    fileprivate var _frameCount: UInt64 = 0
+    fileprivate var _queue: DispatchQueue = DispatchQueue(label: "com.patrickpiemonte.VideoWriter",
+                                                            qos: .userInitiated,
+                                                            autoreleaseFrequency: .workItem,
+                                                            target: DispatchQueue.global())
+    
+    fileprivate var _bufferCount: Int64 = 0
+    fileprivate var _assetWriter: AVAssetWriter?
+    fileprivate var _assetWriterVideoInput: AVAssetWriterInput?
+    fileprivate var _assetWriterPixelBufferInputAdapter: AVAssetWriterInputPixelBufferAdaptor?
+    
+    // MARK: - object lifecycle
+    
+    public init() {
+    }
+
+}
+
+extension VideoWriter {
+
+    fileprivate func createPixelBuffer(fromImage image: UIImage) -> CVPixelBuffer? {
+        autoreleasepool { () -> CVPixelBuffer? in
+            guard let pixelBufferPool = self._assetWriterPixelBufferInputAdapter?.pixelBufferPool else {
+                return nil
+            }
+            
+            var buffer: CVPixelBuffer? = nil
+            guard CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, pixelBufferPool, &buffer) == kCVReturnSuccess else {
+                return nil
+            }
+
+            guard let pixelBuffer = buffer else {
+                return nil
+            }
+                    
+            CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
+            
+            let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer)
+            let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+            let contextSize = image.size
+            
+            if let context = CGContext(data: pixelData,
+                                       width: Int(contextSize.width),
+                                       height: Int(contextSize.height),
+                                       bitsPerComponent: 8,
+                                       bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer),
+                                       space: rgbColorSpace,
+                                       bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) {
+              var imageWidth = image.size.width
+              var imageHeight = image.size.height
+
+              if Int(imageHeight) > context.height {
+                imageHeight = 16 * (CGFloat(context.height) / 16).rounded(.awayFromZero)
+              } else if Int(imageWidth) > context.width {
+                imageWidth = 16 * (CGFloat(context.width) / 16).rounded(.awayFromZero)
+              }
+              
+              context.clear(CGRect(x: 0.0, y: 0.0, width: imageWidth, height: imageHeight))
+              context.setFillColor(UIColor.black.cgColor)
+              context.fill(CGRect(x: 0.0, y: 0.0, width: CGFloat(context.width), height: CGFloat(context.height)))
+              context.concatenate(.identity)
+              
+              if let cgImage = image.cgImage {
+                context.draw(cgImage, in: CGRect(x: 0, y: 0, width: imageWidth, height: imageHeight))
+              }
+              
+              CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
+            }
+            
+            return pixelBuffer
+        }
+    }
+    
+}
+
+extension VideoWriter {
+    
+    fileprivate func setupVideoWriterIfNecessary(withImage image: UIImage) {
+          guard self._assetWriter == nil,
+                let outputFileUrl = self.outputFileUrl,
+                self.outputSize != .zero else {
+              return
+          }
+          
+          do {
+              self._assetWriter = try AVAssetWriter(outputURL: outputFileUrl, fileType: AVFileType.mp4)
+          } catch let error {
+              print("failed \(error)")
+              return
+          }
+          
+          guard let assetWriter = self._assetWriter else {
+              return
+          }
+
+          let outputSettings: [String: Any] = [ AVVideoCodecKey : AVVideoCodecType.h264,
+                                                AVVideoWidthKey : outputSize.width,
+                                                AVVideoHeightKey : outputSize.height]
+          self._assetWriterVideoInput = AVAssetWriterInput(mediaType: .video, outputSettings: outputSettings)
+          if let assetWriterVideoInput = self._assetWriterVideoInput {
+              assetWriterVideoInput.expectsMediaDataInRealTime = true
+
+              let sourcePixelBufferAttributes: [String: Any] = [ kCVPixelBufferPixelFormatTypeKey as String : kCVPixelFormatType_32ARGB,
+                                                                 kCVPixelBufferWidthKey as String : outputSize.width,
+                                                                 kCVPixelBufferHeightKey as String : outputSize.height,
+                                                                 kCVPixelBufferCGImageCompatibilityKey as String : NSNumber(booleanLiteral: true),
+                                                                 kCVPixelBufferCGBitmapContextCompatibilityKey as String : NSNumber(booleanLiteral: true) ]
+
+              self._assetWriterPixelBufferInputAdapter = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterVideoInput,
+                                                                                       sourcePixelBufferAttributes: sourcePixelBufferAttributes)
+              assetWriter.add(assetWriterVideoInput)
+              self._firstFrame = CFAbsoluteTimeGetCurrent()
+          }
+
+          if assetWriter.startWriting() {
+              assetWriter.startSession(atSourceTime: CMTime.zero)
+          }
+    }
+    
+    public func writeFrame(image: UIImage) {
+        self._queue.async {
+            self.setupVideoWriterIfNecessary(withImage: image)
+            if self._assetWriterVideoInput?.isReadyForMoreMediaData ?? false {
+                let presentationTime = CMTime(value: self._bufferCount, timescale: self.framesPerSecond)
+                if let pixelBuffer = self.createPixelBuffer(fromImage: image) {
+                    self._assetWriterPixelBufferInputAdapter?.append(pixelBuffer, withPresentationTime: presentationTime)
+                    self._bufferCount = self._bufferCount + 1
+                }
+            }
+        }
+    }
+    
+    public func finish(completionHandler: (()-> Void)? = nil) {
+        self._queue.async {
+            self._assetWriterVideoInput?.markAsFinished()
+            self._assetWriter?.finishWriting(completionHandler: {
+                self._assetWriterVideoInput = nil
+                self._assetWriterPixelBufferInputAdapter = nil
+                DispatchQueue.main.async {
+                    completionHandler?()
+                }
+            })
+        }
+    }
+}
